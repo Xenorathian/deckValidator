3942ecaafd4941d7d0770c104d70fd695ed789a5
Xenorathian <git!w0rd>
2018-01-27 12:59:43 +0100

    initial files

--- src/main/java/company/Validator.java
+++ src/main/java/company/Validator.java
@@ -22,50 +22,48 @@ public class Validator {
     public static void main(String[] args) {
         List<File> csvFiles = Optional.ofNullable(new File("").getAbsoluteFile()
                 .listFiles(file -> file.getName().contains(".csv"))).map(Arrays::asList).orElse(new ArrayList<>());
-        if(csvFiles.isEmpty()) xenoLog("No CSV files found. Please ensure the CSV file is in the same folder as the jar");
+        if (csvFiles.isEmpty())
+            xenoLog("No CSV files found. Please ensure the CSV file is in the same folder as the jar");
         else csvFiles.forEach(file -> {
-                    try {
-                        xenoLog("PROCESSING FILE: " + file.getName());
-                        Stream<String> stream = Files.lines(Paths.get(file.getName()));
-                        //if(stream.findFirst())
+            try {
+                xenoLog("PROCESSING FILE: " + file.getName());
+                Stream<String> stream = Files.lines(Paths.get(file.getName()));
+                //if(stream.findFirst())
 
-                        stream.skip(1).map(s -> s.replaceAll("\"", ""))
-                                .filter(Validator::hasAnyInformation).forEach(Validator::validate);
-                        xenoLog("FILE DONE: " + file.getName());
-                    } catch (IOException e) {
-                        xenoLog("PROCESSING ERROR: " + file.getName());
-                    }
-                });
+                stream.skip(1).map(s -> s.replaceAll("\"", ""))
+                        .filter(Validator::hasAnyInformation).forEach(Validator::validate);
+                xenoLog("FILE DONE: " + file.getName());
+            } catch (IOException e) {
+                xenoLog("PROCESSING ERROR: " + file.getName());
+            }
+        });
     }
 
     private static void validate(String data) {
         List<String> validLinks = Arrays.stream(data.split("\\,")).filter(Validator::isEternalWarcyLink).collect(Collectors.toList());
-        if(validLinks.isEmpty())
-        {
+        if (validLinks.isEmpty()) {
             xenoLog("No Eternal deck links detected in: \n" + data);
         }
         //TODO:: check exact number of links are present ... will need passed input of number of expected links
-        else
-        {
+        else {
             List<String> nonTournamentDecks = validLinks.stream().filter(Validator::isNotTournamentDeck).collect(Collectors.toList());
-            if(!nonTournamentDecks.isEmpty())
+            if (!nonTournamentDecks.isEmpty())
                 xenoLog("Decklink(s) \n" + nonTournamentDecks.stream().collect(Collectors.joining("\n")) +
-                "\nAre not valid tournament decks from:\n" + data.replaceAll(ETERNAL_LINK.pattern(), "..."));
+                        "\nAre not valid tournament decks from:\n" + data.replaceAll(ETERNAL_LINK.pattern(), "..."));
             else
                 System.out.print(".");
         }
     }
 
-    private static void xenoLog(String message)
-    {
+    private static void xenoLog(String message) {
         System.out.println("\n---===---\n" + message);
     }
 
-    private static Boolean isEternalWarcyLink (String link) {
+    private static Boolean isEternalWarcyLink(String link) {
         return ETERNAL_LINK.matcher(link).matches();
-        }
+    }
 
-    private static Boolean isNotTournamentDeck (String link) {
+    private static Boolean isNotTournamentDeck(String link) {
         return !RestAssured.given().get(link).getBody().asString().contains(TOURNEY_IDENTIFIER);
     }
 
